a9b5578232bbedce579c3484117fd4c9502b22f5
Taylor W <twatanabe02021@gmail.com>
2023-02-08 17:59:06 -0800

    Assignment 2-2

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+
+    Cel = int(input('Please enter a temperature in Celsius:'))
+    Fahr = (9 / 5) * Cel + 32
+    print(f'The temperature in Fahrenheit is: {Fahr:.2f}')
+
     pass
 
 
